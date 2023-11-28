b4cb539e23ea874a2e1162457a568ef4ba91c530
oussala-belhaj-rhouma <oussama.belhajrhouma@etudiant-fst.utm.tn>
2023-11-29 00:14:34 +0100

    connecting to db-2

--- costumer/src/main/java/com/mycompany/app/Costumer.java
+++ costumer/src/main/java/com/mycompany/app/Costumer.java
@@ -1,11 +1,27 @@
 package com.mycompany.app;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Costumer {
+    @Id
+    @SequenceGenerator(
+            name = "costumer_id_sequence",
+            sequenceName = "costumer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "costumer_id_sequence"
+    )
     private Long id;
     private String firstName;
     private String lastName;
--- costumer/src/main/java/com/mycompany/app/CostumerService.java
+++ costumer/src/main/java/com/mycompany/app/CostumerService.java
@@ -3,12 +3,13 @@ package com.mycompany.app;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CostumerService() {
+public record CostumerService(CostumerRepo costumerRepo) {
     public void register(CostumerRegistrationRequest request){
     Costumer costumer =Costumer.builder()
             .firstName(request.firstName())
             .lastName(request.lastName())
             .email(request.email())
             .build();
+    costumerRepo.save(costumer);
         }
 }
